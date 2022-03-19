f6592cac388531da2e052617d9ed3beb3ac23ccc
pauldavidson <pashdoctor@gmail.com>
2022-03-19 19:02:15 -0400

    fix ticket model and routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
@@ -11,14 +11,15 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API'})
+  res.status(200).json({ message: 'Welcome to the Support Desk API' })
 })
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 app.use(errorHandler)
 
