e9210662f1ba74162fb1d0740d28898831bc607a
Pashovski <pashdoctor@gmail.com>
2022-03-24 15:18:27 -0400

    Single ticket display

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/tickets'
+const API_URL = '/api/tickets/'
 
 // Create new ticket
 const createTicket = async (ticketData, token) => {
@@ -23,12 +23,26 @@ const getTickets = async (token) => {
   }
 
   const response = await axios.get(API_URL, config)
+
+  return response.data
+}
+
+// Get user ticket
+const getTicket = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+
+  const response = await axios.get(API_URL + ticketId, config)
   return response.data
 }
 
 const ticketService = {
   createTicket,
   getTickets,
+  getTicket,
 }
 
 export default ticketService
